18e8e806dd61d9401a814fa4688180a943fb5630
iheb1919 <ihebmejri14@gmail.com>
2021-06-27 20:43:42 +0200

    try2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+var $ = require( "jquery" );
+const _ = require('lodash');
+import '../css/main.css'
+$(function() {
+    $('body').append("<span id='logo'></span>");
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="#btn">Click here to get started</button>');
+    $('body').append("<p id='count'></p>");
+	$('body').append('<p>Copyright - Holberton School</p>');
+    $('button').on('click', _.debounce(updateCounter));
+
+    let count = 0;
+    function updateCounter(){
+        count += 1;
+	    $('#count').html(`${count} clicks on the button`);
+    };
+});
