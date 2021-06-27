a398213ba4e451f290cbea9a6c1d8337de482c46
iheb1919 <ihebmejri14@gmail.com>
2021-06-27 20:02:30 +0200

    try

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -6,11 +6,11 @@ $(function() {
     $('body').append('<button id="#btn">Click here to get started</button>');
     $('body').append("<p id='count'></p>");
 	$('body').append('<p>Copyright - Holberton School</p>');
-    $('button').on('click', _.debounce(updateCounter));
 
-let count = 0;
-function updateCounter(){
-    count += 1;
-	$('#count').html(`${count} clicks on the button`);
-};
+    let count = 0;
+    function updateCounter(){
+        count += 1;
+	    $('#count').html(`${count} clicks on the button`);
+    };
+    $('button').on('click', _.debounce(updateCounter));
 });
