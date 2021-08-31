86eb3d9df896279072a7982231a29b068b588af3
iheb1919 <ihebmejri14@gmail.com>
2021-08-31 16:14:05 +0200

    tt

--- 0x07-React_Immutable/main.js
+++ 0x07-React_Immutable/main.js
@@ -1,10 +1,27 @@
-import accessImmutableObject from './2-nested.js'
+import mergeDeeplyElements from './6-deeply.js'
 
 
-  console.log(accessImmutableObject({
-	name: {
-		 first: "Guillaume",
-		 last: "Salva"
-	}
-}, ['name', 'fiwrst']))
+const page1 = {
+	'user-1': {
+	  id: 1,
+	  name: 'test',
+	  likes: {
+		1: {
+		  uid: 1234,
+		}
+	  }
+	},
+  };
+  
+  const page2 = {
+	'user-1': {
+	  likes: {
+		2: {
+		  uid: 134,
+		}
+	  }
+	},
+  };
+  
+  mergeDeeplyElements(page1, page2).toJS();
   
