6fbd6b149f7d82a5b9d2ccb3fa7386103bfd3ddd
iheb1919 <ihebmejri14@gmail.com>
2021-08-31 16:17:09 +0200

    tt

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,4 +1,4 @@
 import {Map} from 'immutable';
 export default function mergeDeeplyElements(page1, page2) {
-	return (Map(page1).mergeDeep(Map(page2)));
+	return (new Map(page1).mergeDeep(new Map(page2)));
 }
